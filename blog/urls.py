61a5e5bdd3f1dadcd26acc1a9aa747e9323f4603
mahayoub123 <mahmoudayoub180699@gmail.com>
2026-03-17 22:12:44 +0200

    finishing the project

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('login')), 
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
     path('home/', views.home_view, name='home'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('add/', views.add_post, name='add_post'),
+    path('edit/<int:id>/', views.edit_post, name='edit_post'),
+    path('delete/<int:id>/', views.delete_post, name='delete_post'),
+    path('logout/', views.logout_view, name='logout'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect ,get_object_or_404
 from .models import Posts
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 def login_view(request):
@@ -42,9 +43,59 @@ def signup_view(request):
     return render(request, 'blog/signup.html')
 
 
-# Home view
 def home_view(request):
     context = {
         'posts': Posts.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+
+
+
+def dashboard(request):
+    posts = Posts.objects.all()
+    return render(request, 'blog/dashboard.html', {'posts': posts})
+
+
+
+@login_required
+def add_post(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+
+        post = Posts.objects.create(
+            title=title,
+            content=content,
+            author=request.user   
+        )
+
+        return redirect('home')
+
+    return render(request, 'blog/add_post.html')
+
+
+def edit_post(request, id):
+    post = get_object_or_404(Posts, id=id)
+
+    if request.method == 'POST':
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('dashboard')
+
+    return render(request, 'blog/edit_post.html', {'post': post})
+
+
+
+def delete_post(request, id):
+    post = get_object_or_404(Posts, id=id)
+    post.delete()
+    return redirect('dashboard')
+
+
+def root(request):
+    return redirect('dashboard')
+def logout_view(request):
+    logout(request)  
+    return redirect('login') 
